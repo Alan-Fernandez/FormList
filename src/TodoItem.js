ca9d45bfe36d3ab52c6a1605e8880792361a8382
AlanFerPcNet <fernandeezalan20@gmail.com>
2022-11-23 09:55:58 -0300

    Completando y eliminando TODOs

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,18 +2,11 @@ import React, { Fragment } from "react";
 import "./TodoItem.css"
 
 const TodoItem =(props)=>{
-    const onComplete =()=>{
-        alert('completado: ' + props.text)
-    }
-
-    const onDelete =()=>{
-        alert('eliminado: '+ props.text)
-    }
     return(
         <Fragment>
             <li className="TodoItem">
                 <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
-                onClick={onComplete}
+                onClick={props.onComplete}
                 >
                     √
                 </span>
@@ -24,7 +17,7 @@ const TodoItem =(props)=>{
 
                 <span 
                     className="Icon Icon-delete"
-                    onClick={onDelete}
+                    onClick={props.onDelete}
                     >
                     X
                     </span>
